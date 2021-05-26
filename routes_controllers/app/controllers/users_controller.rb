7cb18101e6389cd9682f92d114ae54d21b3c23cd
Samuel Sacco <SamLSacco@aol.com>
2021-05-26 16:43:33 -0400

    finished routes_and_controllers

--- routes_controllers/app/controllers/users_controller.rb
+++ routes_controllers/app/controllers/users_controller.rb
@@ -3,17 +3,37 @@ class UsersController < ApplicationController
         @users = User.all
         render json: @users
     end
-
+    
     def create
-        user = User.new(params.require(:user).permit(:name, :email))
-        if user.save 
-            render json: user
+        @user = User.new(user_params)
+        if @user.save 
+            render json: @user
         else   
-            render json: user.errors.full_messages, status: :unprocessable_entity
+            render json: @user.errors.full_messages, status: :unprocessable_entity
+        end
+    end
+    
+    def show
+        @user = User.find(params[:id])
+        render json: @user
+    end
+    
+    def update
+        @user = User.find(params[:id])
+        if @user.update(user_params)
+            redirect_to user_url(@user)
+        else
+            render json: @user.errors.full_messages, status: :unprocessable_entity
         end
     end
 
-    def show
-        render json: params
+    def user_params
+        params.require(:user).permit(:name, :email)
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to users_url
     end
 end
--- routes_controllers/config/routes.rb
+++ routes_controllers/config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # resources :users
   get '/users', to: 'users#index'
-  get 'users/book', to: 'users#book'
   get 'users/:id', to: 'users#show', as: 'user'
   get 'users/new', to: 'users#new', as: 'new_user'
-
-
-  post '/users', to: 'users#create'
   
+  post '/users', to: 'users#create'
+
+  patch '/users/:id', to: 'users#update'
+
+  delete 'users/:id', to: 'users#destroy'
 end
